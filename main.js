2dfd53e7fb92014a8c9d1b5cd94579dfdb28c9fa
Glenn Harris <harris.glenn.jr@gmail.com>
2017-01-09 16:54:43 -0800

    added jquery and it works

--- main.js
+++ main.js
@@ -2,4 +2,10 @@
   'use strict';
 
   console.log('hello world!');
+  const target = $('.jqueryTest');
+  const styles = {
+    border: '1px solid black',
+    height: '250px',
+  };
+  target.css(styles);
 }());
