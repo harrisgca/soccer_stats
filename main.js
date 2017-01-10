01c8c40c342f631c86e2ef4ce8c80cdbe8eec8a1
Glenn Harris <harris.glenn.jr@gmail.com>
2017-01-09 17:38:22 -0800

    map key codes to actions

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 (function() {
   'use strict';
 
-  console.log('hello world!');
-  const target = $('.jqueryTest');
-  const styles = {
-    border: '1px solid black',
-    height: '250px',
+  const buttonPressedContainer = $('.buttonPressed');
+  const actionRecordedContainer = $('.actionRecorded');
+
+  /**
+    - Goals
+    - Shots
+    - Shots on target
+    - Corner kicks
+    - Free Kicks
+    - Offsides
+    - Pass completion percentage
+  **/
+  const teamKeycodes = {
+    // our team actions
+    97:{button:'a', action: 'Shot off target - Us'},
+    115:{button:'s', action: 'Shot on Target - Us'},
+    100:{button:'d', action: 'Incomplete Pass - Us'},
+    102:{button:'f', action: 'Completed Pass - Us'},
+    103:{button:'g', action: 'Goal Scored - Us'},
+    // other team actions
+    104:{button:'h', action: 'Goal Scored - Them'},
+    59:{button:';', action: 'Shot off target - Them'},
+    108:{button:'l', action: 'Shot on Target - Them'},
+    107:{button:'k', action: 'Incomplete Pass - Them'},
+    106:{button:'j', action: 'Completed Pass - them'},
   };
-  target.css(styles);
+
+  const displayLastButtonPressed = function _displayLastButtonPressed(e){
+    console.log('keypress logged');
+    console.log(e.which);
+    console.log(teamKeycodes[e.which]);
+    if (teamKeycodes[e.which]) {
+      buttonPressedContainer.html(`${teamKeycodes[e.which].button}`);
+      actionRecordedContainer.html(`${teamKeycodes[e.which].action}`);
+      return false;
+    }else{
+      buttonPressedContainer.html('');
+      actionRecordedContainer.html('');
+      return false;
+    }
+
+  };
+
+  $(window).on('keypress', displayLastButtonPressed);
 }());
